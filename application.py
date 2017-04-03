6c65112fc92047681643114f9f0e32d324f52df9
Minghua Jiang <61221630@qq.com>
2017-04-02 21:06:14 -0400

    　add rate limit

--- application.py
+++ application.py
@@ -2,32 +2,29 @@ import logging
 from flask import Flask, request
 import rate_limit
 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 application = Flask(__name__)
 
-@application.after_request
-def inject_x_rate_headers(response):
-    limit = rate_limit.get_view_rate_limit()
-    if limit and limit.send_x_headers:
-        h = response.headers
-        h.add('X-RateLimit-Remaining', str(limit.remaining))
-        h.add('X-RateLimit-Limit', str(limit.limit))
-        h.add('X-RateLimit-Reset', str(limit.reset))
-    return response
+limiter = rate_limit.get_limiter(application)
+
 
 @application.route('/rate-limited')
-@rate_limit.ratelimit(limit=10, per=60 * 1)
+@limiter.limit("10 per hour")
 def test():
     return "this is rate limit test"
 
 
 @application.route('/')
+@limiter.exempt
 def index():
     return "hello world!"
 
+
 @application.route('/rate')
+@limiter.limit("5 per hour")
 def rate():
     return "test"
 
