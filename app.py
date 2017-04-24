4390ba3e2d4ece153bf0ba5e0cc72ee3925de1a8
Minghua Jiang <61221630@qq.com>
2017-04-24 01:29:57 -0400

    update code

--- app.py
+++ app.py
@@ -1,58 +1,82 @@
 import logging
 import os
 from flask import Flask, request
-import rate_limit
 from engine import RecommendationEngine
 import json
+from dao import MovieRatingDao
+import dataset_updater
 from pyspark import SparkContext, SparkConf
 
-
-def init_spark_context():
-    # load spark context
-    conf = SparkConf().setAppName("movielens_recommendation-server")
-    # IMPORTANT: pass aditional Python modules to each worker
-    sc = SparkContext(conf=conf, pyFiles=['engine.py', 'application.py'])
-    return sc
-
-sc = init_spark_context()
-dataset_path = os.path.join('datasets', 'ml-latest-small')
-
-recommendation_engine = RecommendationEngine(sc, dataset_path)
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 application = Flask(__name__)
-
-limiter = rate_limit.get_limiter(application)
+logger = logging.getLogger(__name__)
 
 
 @application.route("/<user_id>/ratings/top/<int:count>", methods=["GET"])
-def top_ratings(user_id, count):
+def top_recommendation(user_id, count):
     logger.debug("User %s TOP ratings requested", user_id)
     top_ratings = recommendation_engine.get_top_ratings(user_id, count)
     return json.dumps(top_ratings)
 
 
 @application.route("/<user_id>/ratings/<int:movie_id>", methods=["GET"])
-def movie_ratings(user_id, movie_id):
+def predict_rating(user_id, movie_id):
     logger.debug("User %s rating requested for movie %s", user_id, movie_id)
     ratings = recommendation_engine.get_ratings_for_movie_ids(user_id, [movie_id])
     return json.dumps(ratings)
 
 
-@application.route("/<user_id>/ratings", methods=["POST"])
-def add_ratings(user_id):
+@application.route("/rating/<user_id>/<int:movie_id>/<float:rating>", methods=["GET", "POST"])
+def add_comment_and_rating(user_id, movie_id, rating):
     # get the ratings from the Flask POST request object
-    ratings_list = request.form.keys()[0].strip().split("\n")
-    ratings_list = map(lambda x: x.split(","), ratings_list)
-    # create a list with the format required by the negine (user_id, movie_id, rating)
-    ratings = map(lambda x: (user_id, int(x[0]), float(x[1])), ratings_list)
-    # add them to the model using then engine API
-    recommendation_engine.add_ratings(ratings)
-    return json.dumps(ratings)
+    #user_id = 0
+    #movie_id = request.form.movie_id
+    #comments = request.form.comment
+    #rating = request.form.rating
+    if not recommendation_engine.check_if_rated(user_id, movie_id):
+        dao.add_comments_rating(user_id, movie_id, "", rating)
+        recommendation_engine.add_rating([(user_id, movie_id, rating)])
+        return "added rating successfully"
+    else:
+        return "already rated"
 
+
+@application.route("/rating/<user_id>/<int:movie_id>", methods=["GET"])
+def check_rated(user_id, movie_id):
+    result = dict()
+    result['rated'] = recommendation_engine.check_if_rated(user_id, movie_id)
+    return json.dumps(result)
+
+
+@application.route("/model", methods=["GET", "POST"])
+def retrain_model():
+    recommendation_engine.retrain_model()
+    return "model re-trained successfully"
+
+
+@application.route("/datasets", methods=["GET", "POST"])
+def update_dataset():
+    if dataset_updater.update_dataset():
+        recommendation_engine.load_dataset(dao.get_user_ratings())
+        return "dataset updated successfully"
+    else:
+        return "no changes in dataset"
+
+
+def init_spark_context():
+    # load spark context
+    conf = SparkConf().setAppName("movielens_recommendation")
+    # IMPORTANT: pass additional Python modules to each worker
+    sc = SparkContext(conf=conf, pyFiles=['engine.py'])
+    return sc
 
 if __name__ == '__main__':
-	application.debug = True
-	application.run()
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+    sc = init_spark_context()
+    dataset_path = os.path.join('datasets', 'ml-latest-small')
+    dao = MovieRatingDao()
+    recommendation_engine = RecommendationEngine(sc, dataset_path, dao.get_user_ratings())
+
+    application.debug = False
+    application.run()
+
