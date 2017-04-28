57af7afc3c6a6042eeda40bcbede5c7bb0cfff6f
Minghua Jiang <61221630@qq.com>
2017-04-28 14:26:57 -0400

    fix bug

--- dao.py
+++ dao.py
@@ -10,16 +10,3 @@ class UserDao:
 
     def find_user(self, username):
         return self.db.user.find_one({'username': username})
-
-
-class MockUserDao:
-    def __init__(self):
-        self.db = None
-
-    def add_new_user(self, username, email, password):
-        return True
-
-    def find_user(self, username):
-        result = dict()
-        result["username"] = 'huazai'
-        return result
