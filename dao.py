8026deca2cb1d04db231e6f184b6f565ff525a58
Minghua Jiang <61221630@qq.com>
2017-04-29 03:35:00 -0400

    update dao

--- dao.py
+++ dao.py
@@ -6,7 +6,11 @@ class UserDao:
         self.db = MongoClient('184.73.28.43', 27017)['movie-lens']
 
     def add_new_user(self, username, email, password):
-        self.db.user.insert({'username': username, 'email':email, 'password':password})
+        self.db.user.insert({'username': username, 'email':email, 'password': password, 'isAdmin': False})
 
     def find_user(self, username):
         return self.db.user.find_one({'username': username})
+
+
+if __name__ == '__main__':
+    userDao = UserDao()
